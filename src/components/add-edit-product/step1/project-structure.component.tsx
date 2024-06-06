7695c37b7189b750cf9d1509f006f223dee2f2cb
Savan Patel <35634271+SavanAghera@users.noreply.github.com>
2024-06-06 13:38:45 +0000

    added btns

--- src/components/add-edit-product/step1/project-structure.component.tsx
+++ src/components/add-edit-product/step1/project-structure.component.tsx
@@ -8,6 +8,7 @@ import { useEffect, useCallback } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { fetchProjectDetail, updateProjectData, updatebuildingAreaData } from "../../../store/fearure/project-detail.slice";
 import { updateCurrentStep, updateIsStepVisible } from "../../../store/fearure/project-step.slice";
+import { Button } from "primereact/button";
 
 const ProjectStructureComponent = () => {
     const projectDetailState = useAppSelector(
@@ -161,96 +162,115 @@ const ProjectStructureComponent = () => {
         }
     }, []);
     return (
-        <div className="">
-
-            <section className="">
-                <div className="">
-                    <div className="">
-                        <h2>Define building areas</h2>
-                    </div>
-                    <p className="text-500 text-lg -mt-5">
+        <div className="flex justify-content-around mt-3">
+            <div>
+                <section className="mt-4">
+                    <div className="m-auto max-w-max text-4xl font-semibold">Define building areas</div>
+                    <p className="text-500 text-lg mt-3">
                         Choose the areas included in your building by selecting from the
                         options below. These are grouped by Indoor and Outdoor locations.
                     </p>
-                </div>
-            </section>
-            {
-                projectDetailState?.projectDetail?.buildingAreas?.map((buildingArea, buildingAreaIndex) => (
-                    <section className="flex w-full justify-content-center p-5" key={buildingArea.name}>
+                </section>
+                {
+                    projectDetailState?.projectDetail?.buildingAreas?.map((buildingArea, buildingAreaIndex) => (
+                        <section className="flex w-full justify-content-center p-5" key={buildingArea.name}>
 
-                        <div style={{ width: "58rem" }}>
-                            <div className="w-full">
-                                <span className="text-2xl font-semibold">{buildingArea.name}</span>
+                            <div style={{ width: "58rem" }}>
+                                <div className="w-full">
+                                    <span className="text-2xl font-semibold">{buildingArea.name}</span>
 
-                                <Divider className="m-0 mt-4" />
-                            </div>
-                            <div className="flex flex-column w-full mt-3">
-                                {buildingArea?.areas?.map(
-                                    (area, index) => {
-                                        return (
-                                            <div
-                                                className="flex justify-content-between align-items-center  h-4rem"
-                                                key={area.name + "_" + index}
-                                            >
-                                                <div className="w-14rem">
-                                                    <Checkbox
-                                                        onChange={(e) => {
+                                    <Divider className="m-0 mt-4" />
+                                </div>
+                                <div className="flex flex-column w-full mt-3">
+                                    {buildingArea?.areas?.map(
+                                        (area, index) => {
+                                            return (
+                                                <div
+                                                    className="flex justify-content-between align-items-center  h-4rem"
+                                                    key={area.name + "_" + index}
+                                                >
+                                                    <div className="w-14rem">
+                                                        <Checkbox
+                                                            onChange={(e) => {
+                                                                dispatch(
+                                                                    updatebuildingAreaData({
+                                                                        buildingAreaIndex,
+                                                                        index,
+                                                                        value: { ...area, isSelected: e.checked },
+                                                                    })
+                                                                );
+                                                            }}
+                                                            checked={area.isSelected || false}
+                                                        ></Checkbox>
+                                                        <span className="text-xl font-semibold text-600">
+                                                            {" "}
+                                                            {area.internalName}
+                                                        </span>
+                                                    </div>
+                                                    <InputText
+                                                        placeholder={area.name}
+                                                        value={area.name}
+                                                        onChange={(e) =>
                                                             dispatch(
                                                                 updatebuildingAreaData({
                                                                     buildingAreaIndex,
                                                                     index,
-                                                                    value: { ...area, isSelected: e.checked },
+                                                                    value: { ...area, name: e.target?.value },
                                                                 })
-                                                            );
-                                                        }}
-                                                        checked={area.isSelected || false}
-                                                    ></Checkbox>
-                                                    <span className="text-xl font-semibold text-600">
-                                                        {" "}
-                                                        {area.internalName}
-                                                    </span>
+                                                            )
+                                                        }
+                                                        autoFocus
+                                                    />
+                                                    <InputText
+                                                        className="w-18rem"
+                                                        placeholder="Additional Comment (Optional)"
+                                                        value={area.description}
+                                                        onChange={(e) =>
+                                                            dispatch(
+                                                                updatebuildingAreaData({
+                                                                    buildingAreaIndex,
+                                                                    index,
+                                                                    value: { ...area, description: e.target?.value },
+                                                                })
+                                                            )
+                                                        }
+                                                    />
                                                 </div>
-                                                <InputText
-                                                    placeholder={area.name}
-                                                    value={area.name}
-                                                    onChange={(e) =>
-                                                        dispatch(
-                                                            updatebuildingAreaData({
-                                                                buildingAreaIndex,
-                                                                index,
-                                                                value: { ...area, name: e.target?.value },
-                                                            })
-                                                        )
-                                                    }
-                                                    autoFocus
-                                                />
-                                                <InputText
-                                                    className="w-18rem"
-                                                    placeholder="Additional Comment (Optional)"
-                                                    value={area.description}
-                                                    onChange={(e) =>
-                                                        dispatch(
-                                                            updatebuildingAreaData({
-                                                                buildingAreaIndex,
-                                                                index,
-                                                                value: { ...area, description: e.target?.value },
-                                                            })
-                                                        )
-                                                    }
-                                                />
-                                            </div>
-                                        );
-                                    }
-                                )}
-                                <b className="text-primary mt-3">
-                                    <i className="pi pi-plus" /> Add another
-                                </b>
+                                            );
+                                        }
+                                    )}
+                                    <b className="text-primary mt-3">
+                                        <i className="pi pi-plus" /> Add another
+                                    </b>
+                                </div>
                             </div>
-                        </div>
-                    </section>)
-                )
-            }
+                        </section>)
+                    )
+
+                }
+                <div className="flex justify-content-between">
+                    <div>
+                        <Button label="Back" icon="pi pi-angle-left" rounded  severity="secondary" size="large"/>
+                    </div>
+                    <div>
+                        {
+                            [1,2,3].map(v => (
+                                <div  style={{
+                                    height: '0.625rem',
+                                    width: '0.625rem',
+                                    borderRadius: '50%',
+                                    background:'blue'
+                                }}></div>
+                            ))
+                        }
+                    </div>
+                    <div>
+                        <Button label="Define Floors" icon="pi pi-angle-right" rounded iconPos="right" size="large"/>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
